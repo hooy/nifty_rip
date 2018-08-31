df8449016afa5ea7176ca8751bbd088f6a86a798
peaceda <sergejs.gorbunovs@tele2.com>
2018-08-31 18:05:35 +0300

    initial models, just to sync code from work to continue work on holidays

--- app/Product.php
+++ app/Product.php
@@ -4,7 +4,29 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+  * price
+  * product_type_id
+  * product_color_id
+  * product_size_id
+*/
+
 class Product extends Model
 {
-    //
+    public function color() {
+       return $this->belongsTo('App\ProductColor');
+    }
+
+    public function size() {
+       return $this->belongsTo('App\ProductSize');
+    }
+
+    public function type() {
+       return $this->belongsTo('App\ProductType');
+    }
+
+    public function currency() {
+       return $this->belongsTo('App\Currency');
+    }
+
 }
--- database/migrations/2018_08_27_142925_create_products_table.php
+++ database/migrations/2018_08_27_142925_create_products_table.php
@@ -11,7 +11,7 @@ class CreateProductsTable extends Migration
      *
      * @return void
      * at this point lets have simple model, with straightforward
-     * implementation of currency fields;
+     * implementation of price and currency fields;
      * in future good to have smart model with some helper functions
      * to achieve multi-currency fields with rates, history etc.
      * or at least have separate model for "money" field and add
@@ -21,9 +21,30 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
-            $table->timestamps();
+
             $table->decimal('price', 12, 2);
-            $table->enum('currency', ['USD', 'EUR']);
+
+            $table->foreign('currency_id')
+                  ->references('id')
+                  ->on('currencies')
+                  ->onDelete('restrict');
+
+            $table->foreign('product_type_id')
+                  ->references('id')
+                  ->on('product_types')
+                  ->onDelete('restrict');
+
+            $table->foreign('product_color_id')
+                  ->references('id')
+                  ->on('product_colors')
+                  ->onDelete('restrict');
+
+            $table->foreign('product_size_id')
+                  ->references('id')
+                  ->on('product_sizes')
+                  ->onDelete('restrict');
+
+            $table->timestamps();
         });
     }
 
